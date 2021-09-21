e071d7eb48463b9791f764a918c1ec14e5cd968c
Bilal-K95 <bilal.khatik@neosoftmail.com>
2021-09-21 21:37:42 +0530

    Calculator Assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+
+    let screen = document.getElementById("screen");
+    buttons = document.querySelectorAll("button");
+    let screenValue = "";
+    for (item of buttons) {
+      item.addEventListener("click", (e) => {
+        buttonText = e.target.innerText;
+        console.log("button text", buttonText);
+        if (buttonText == "X") {
+          buttonText = "*";
+          screenValue += buttonText;
+          screen.value = buttonText;
+        } else if (buttonText == "CE") {
+          screenValue = "";
+          screen.value = screenValue;
+        } else if (buttonText == "=") {
+          screen.value = eval(screenValue);
+        } else {
+          screenValue += buttonText;
+          screen.value = screenValue;
+        }
+      });
+    }
